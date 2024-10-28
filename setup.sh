f8a2d51ee6415241be01546de68af0739bd86a00
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2024-10-28 19:37:55 +0000

    Add simple setup script with conditionals

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+PYTHON_VERSION='3.12'
+DATE='10-24'
+# set env name
+MULE_ENV_NAME=MULE-${PYTHON_VERSION}-${DATE}
+
+# set directory path to variable
+export MULE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "$(<${MULE_DIR}/assets/MULE.txt)"
+
+echo "Identified directory: $MULE_DIR"
+
+# If conda isn't installed, install conda
+if ! which conda >> /dev/null
+then
+	echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	echo "No Conda installation detected, please install conda"
+	echo "https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html#regular-installation"
+	exit 1
+fi
+
+# If conda environment exists, activate it. Otherwise create it
+if ! (conda env list | grep ${MULE_ENV_NAME}) >> /dev/null
+then
+	echo "Couldn't find environment, creating environment..."
+	conda env create -f MULE_environment.yml
+fi
+
+echo "Activating environment..."
+conda activate ${MULE_ENV_NAME}
+
+cd ${MULE_DIR}
