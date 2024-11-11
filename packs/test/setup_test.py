4f3c3a7f0865e79ce6b909051f95abe53c604f0b
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2024-11-11 15:48:09 +0000

    Add test, modify MULE executable

---
+++ packs/test/setup_test.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+from pytest                import mark
+
+
+
+@mark.parametrize("pack", ["acq", "proc", "test"])
+def test_executable_runs_successfully(pack):
+    '''
+    This test is made to check if the current executable method for
+    `bin/mule` to work as intended, accessing the relevant files when run.
+    '''
+    bin_dir = str(os.environ['MULE_DIR'])
+                                                            # config will need to be improved
+    run_pack = ["python3", bin_dir + "/bin/mule", str(pack), "config"]
+
+    # ensure output is successful (no errors)
+
+    # this should be more complex in the future, such as if the config (which
+    # will be a default for the tests) returns the test flag return a number, or
+    # something in particular from subprocess
+    assert subprocess.run(run_pack).returncode == 0
+
+
