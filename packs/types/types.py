a74df91554006d660e2e7368b43d77cc6176beb9
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2024-11-14 16:33:33 +0000

    Add channels column for single channel samples

---
+++ packs/types/types.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+event_info_type = np.dtype([
+            ('event_number', np.uint32), 
+            ('timestamp', np.uint64), 
+            ('samples', np.uint32), 
+            ('sampling_period', np.uint64), 
+            ('channels', np.int32),
+            ])
