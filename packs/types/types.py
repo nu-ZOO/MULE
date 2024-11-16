077f8777cbac32dcb70c6e4829fb838c2efb80e3
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2024-11-16 19:53:38 +0000

    generalise data types for binary collection

--- packs/types/types.py
+++ packs/types/types.py
@@ -16,3 +16,30 @@ def rwf_type(samples):
                 ('rwf', np.float32, (samples,))
     
     ])  
+
+
+def generate_wfdtype(channels, samples):
+    '''
+    generates the dtype for collecting the binary data based on samples and number of
+    channels
+    '''
+    if channels >1:
+        wdtype = np.dtype([
+                ('event_number', np.uint32), 
+                ('timestamp', np.uint64), 
+                ('samples', np.uint32), 
+                ('sampling_period', np.uint64), 
+                ('channels', np.int32),
+                ] + 
+                [(f'chan_{i+1}', np.float32, (samples,)) for i in range(0,channels)]
+        )
+    else:
+        wdtype = np.dtype([
+            ('event_number', np.uint32), 
+            ('timestamp', np.uint64), 
+            ('samples', np.uint32), 
+            ('sampling_period', np.uint64),
+            ('chan_1', np.float32, (samples,))
+        ])
+
+    return wdtype   
