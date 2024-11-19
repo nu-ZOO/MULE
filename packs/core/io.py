f17717bd14ed5dadacc9ca7d0fb9f17f4f21d2ff
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2024-11-19 16:26:29 +0000

    Add executable functionality for WD2 decoding This commit allows for WD2 decoding to be exectured straight from the MULE binary, as well as the default config and config-reading setup. A default config is provided.

--- packs/core/io.py
+++ packs/core/io.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 import h5py
+import ast
+import configparser
+
 
 from packs.types import types
 
@@ -49,3 +52,19 @@ def load_rwf_info(file_path  :  str,
     return pd.DataFrame(list(map(list, h5_data)), columns = (types.rwf_type(samples)).names)
 
 
+def read_config_file(file_path  :  str) -> dict:
+    # setup config parser
+    config = configparser.ConfigParser()
+
+    # read in arguments, require the required ones
+    config.read(file_path)
+    arg_dict = {}
+    for section in config.sections():
+        for key in config[section]:
+            # the config should be written in such a way that the python evaluator
+            # can determine its type
+            #
+            # we can setup stricter rules at some other time
+            arg_dict[key] = ast.literal_eval(config[section][key])
+    
+    return arg_dict
