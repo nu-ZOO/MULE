723ddf34e3d5460b356f448dadf78e4ca87970e6
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2024-11-19 16:26:29 +0000

    Add executable functionality for WD2 decoding This commit allows for WD2 decoding to be exectured straight from the MULE binary, as well as the default config and config-reading setup. A default config is provided.

--- packs/proc/proc.py
+++ packs/proc/proc.py
@@ -1,3 +1,24 @@
-def proc():
-    print("This works as expected: processing")
-    print("In here you should read the config provided")
+import os
+
+from packs.core.io                import read_config_file
+from packs.proc.processing_utils  import process_bin_WD2
+
+
+def proc(config_file):
+    print("Starting the processing pack...")
+
+    # take full path
+    full_path = os.path.expandvars(config_file)
+
+    conf_dict = read_config_file(full_path)
+    # removing the first two components so that they can be fed into functions
+    arg_dict  = dict(list(conf_dict.items())[2:])
+    # check the method implemented, currently just process
+    match conf_dict['process']:
+        case 'decode':
+            if conf_dict['wavedump_edition'] == 2:
+                process_bin_WD2(**arg_dict)
+            else:
+                print(f"wavedump edition {conf_dict['wavedump_edition']} decoding isn't currently implemented.")
+        case default:
+            print(f"process {conf_dict['process']} not currently implemented.")
