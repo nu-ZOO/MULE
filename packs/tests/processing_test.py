6cb31772e32435eb2d3924a0e445ff79b0bd47da
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2025-01-31 17:26:00 +0000

    Add catch for edge case with

--- packs/tests/processing_test.py
+++ packs/tests/processing_test.py
@@ -36,7 +36,7 @@ def test_rwf_type_has_correct_shape(samples):
 
 
 def test_header_components_read_as_expected():
-    
+
     MULE_dir = str(os.environ['MULE_DIR'])
     file = MULE_dir + '/packs/tests/data/three_channels_WD2.bin'
 
@@ -71,6 +71,21 @@ def test_header_processed_correctly():
     assert result[2] == smpl_prd
     assert result[3] == channels
 
+def test_header_works_when_data_malformed():
+    # this test would normally cause a memory error as the data
+    # provided is singular channel, and `process_header()` tests
+    # for single channel behaviour by analysing it as multi-channel
+    # and returning a single channel response if the header breaks.
+
+    # Here,the header will crash out due to a channels value > 10^10.
+    # This has been fixed quickly in process_header, but should be
+    # optimised in a different fashion.
+
+    MULE_dir = str(os.environ['MULE_DIR'])
+    file = MULE_dir + '/packs/tests/data/malformed_data.bin'
+
+    process_header(file)
+
 @mark.parametrize("function, error", [(process_header, NameError),
                                       (read_defaults_WD2, ValueError)])
 def test_endian_error_when_reading(function, error):
@@ -79,8 +94,8 @@ def test_endian_error_when_reading(function, error):
     file = MULE_dir + '/packs/tests/data/three_channels_WD2.bin'
 
 
-    byte_order = 'Big' # this will raise a ValueError 
-    
+    byte_order = 'Big' # this will raise a ValueError
+
     with raises(error):
         with open(file, 'rb') as f:
             holder = function(f, byte_order)
@@ -93,7 +108,7 @@ def test_invalid_file_for_reading():
 
 
     x = read_binary(file, types.generate_wfdtype(1, 1000))
-    
+
     # if you've malformed the data types on a non-binary file, the result should be empty
     # but this may not always be the case.
     assert len(x) == 0
@@ -103,13 +118,13 @@ def test_formatting_works():
 
     MULE_dir = str(os.environ['MULE_DIR'])
 
-    file_path  = MULE_dir + '/packs/tests/data/three_channels_WD2.bin'    
-    
+    file_path  = MULE_dir + '/packs/tests/data/three_channels_WD2.bin'
+
     # collect relevant data from output
     check_file      = MULE_dir + '/packs/tests/data/three_channels_WD2.h5'
     check_rwf       = load_rwf_info(check_file, 1000)
     check_evt_info  = load_evt_info(check_file)
-    
+
 
     channels = 3
     samples = 1000
@@ -119,19 +134,19 @@ def test_formatting_works():
     with open(file_path, 'rb') as file:
         # read in data
         data = read_binary(file, wdtype)
-    
+
     evt_info, rwf = format_wfs(data, wdtype, samples, channels)
 
     # modify into dataframes for appropriate comparison
     rwf = pd.DataFrame(list(map(list, rwf)), columns = rwf_type(samples).names)
     evt_info = pd.DataFrame(list(map(list, evt_info)), columns = event_info_type.names)
-    
+
     assert rwf.equals(check_rwf)
     assert evt_info.equals(check_evt_info)
 
 
 def test_ensure_new_path_created():
-    
+
     MULE_dir = str(os.environ['MULE_DIR'])
     data_path     = MULE_dir + '/packs/tests/data/three_channels_WD2.h5'
     new_data_path = MULE_dir + '/packs/tests/data/three_channels_WD21.h5'
@@ -142,7 +157,7 @@ def test_ensure_new_path_created():
 
 
 def test_runtime_error_when_too_many_save_files():
-    
+
     MULE_dir     = str(os.environ['MULE_DIR'])
     relevant_dir = MULE_dir + '/packs/tests/data/repetitive_data/'
     # generate 101 empty files
@@ -154,12 +169,12 @@ def test_runtime_error_when_too_many_save_files():
     with raises(RuntimeError):
         check_save_path(relevant_dir + 'test_.txt', overwrite=False)
 
-@mark.parametrize("config, inpt, output, comparison", [("process_WD2_1channel.conf", "one_channel_WD2.bin", "one_channel_tmp.h5", "one_channel_WD2.h5"), 
+@mark.parametrize("config, inpt, output, comparison", [("process_WD2_1channel.conf", "one_channel_WD2.bin", "one_channel_tmp.h5", "one_channel_WD2.h5"),
                                            ("process_WD2_3channel.conf", "three_channels_WD2.bin", "three_channels_tmp.h5", "three_channels_WD2.h5")])
 def test_decode_produces_expected_output(config, inpt, output, comparison):
 
     MULE_dir     = str(os.environ['MULE_DIR'])
-    data_dir     = "/packs/tests/data/" 
+    data_dir     = "/packs/tests/data/"
 
     # ensure path is correct
     file_path       = MULE_dir + data_dir + inpt
@@ -185,5 +200,4 @@ def test_decode_produces_expected_output(config, inpt, output, comparison):
     # check that the resulting dataframe is as expected
     assert load_evt_info(save_path).equals(load_evt_info(comparison_path))
     assert load_rwf_info(save_path, samples).equals(load_rwf_info(comparison_path, samples))
-    
 
