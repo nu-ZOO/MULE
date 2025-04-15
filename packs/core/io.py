3fb38f53516decad7035907ea452c7fce8f16987
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2025-04-15 17:02:18 +0100

    add docstrings

--- packs/core/io.py
+++ packs/core/io.py
@@ -115,14 +115,30 @@ def writer(path        :  str,
            group       :  str,
            overwrite   :  Optional[bool] = True) -> Generator:
     '''
-    Standard function that tries to put data into a dataset within a h5 file.
-    It will create everything it needs up the chain (dataset, group, path) when
-    flag 'w' is created.
+    Outer function for a lazy h5 writer that will iteratively write to a dataset, with the formatting:
 
+    FILE.h5 -> GROUP/DATASET
+
+    Includes overwriting functionality, which will overwrite **GROUPS** at will if needed.
+
+    Parameters
+    ----------
+
+    path (str)       :  File path
+    group (str)      :  Group within the h5 file
+    overwrite(bool)  :  Boolean for overwriting previous dataset (OPTIONAL)
+    
+    Returns
+    -------
+
+    write (func)     : write function described in write()
+    
+    
     Fixed size is for when you know the size of the output file, so you set the size
     of the df beforehand, saving precious IO operation. The input then becomes a tuple
     of (True, DF_SIZE, INDEX), otherwise its false.
     '''
+
 
     # open file if exists, create group or overwrite it
     h5f = h5py.File(path, 'a')
@@ -135,8 +151,28 @@ def writer(path        :  str,
 
         def write(dataset     :  str,
                   data        :  np.ndarray,
-                  fixed_size  :  Optional[Union[False, Tuple[True, int, int]]] = False):
+                  fixed_size  :  Optional[Union[False, Tuple[True, int, int]]] = False) -> None:
+            '''
+            Writes ndarray to dataset within group defined in writer().
 
+            Fixed size used to speed up writing, if True will 
+            create a dataset of a fixed size rather than 
+            increasing the size iteratively.
+
+            Parameters
+            ----------
+
+            dataset (str)       :  Dataset name to write to
+            data (ndarray)      :  Data to write*
+            fixed_size (Union[Bool, Tuple[Bool, int, int]])
+                                :  Method that's either enable or disabled.
+                                     False (disabled) -> Iteratively increases size of dataframe at runtime
+                                     True  (enabled)  -> Requires Tuple containing 
+                                                            (True, number of events, index to write to)
+                                   This method is best seen in action in `process_bin_WD1()`.
+
+            * Data should be in a numpy structured array format, as can be seen in WD1 and WD2 processing
+            '''
             if fixed_size is False:
                 # create dataset if doesnt exist, if does make larger
                 if dataset in gr:
@@ -167,12 +203,23 @@ def writer(path        :  str,
 
 def reader(path     :  str,
            group    :  str,
-           dataset  :  str):
+           dataset  :  str) -> Generator:
     '''
-    Standard function that stupidly reads events iteratively from h5 file group.
+    A lazy h5 reader that will iteratively read from a dataset, with the formatting:
+    
+    FILE.H5 -> GROUP/DATASET
 
-    Should be redone with this documentation in mind:
-    https://docs.h5py.org/en/stable/high/dataset.html#reading-writing-data
+    Parameters
+    ----------
+
+    path (str)       :  File path
+    group (str)      :  Group name within the h5 file
+    dataset (str)    :  Dataset name within the group
+
+    Returns
+    -------
+
+    row (generator)  :  Generator object that returns the next row from the dataset upon being called.
     '''
 
     with h5py.File(path, 'r') as h5f:
