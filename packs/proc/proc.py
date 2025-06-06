e0ea1d297a31faa166cfb8cd61af18ffbceac9a9
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2025-06-06 14:10:12 +0100

    include charge and height calculations into processing

--- packs/proc/proc.py
+++ packs/proc/proc.py
@@ -3,6 +3,7 @@ import os
 from packs.core.io                import read_config_file
 from packs.proc.processing_utils  import process_bin_WD2
 from packs.proc.processing_utils  import process_bin_WD1
+from packs.proc.waveform_utils    import calibrate
 from packs.core.core_utils        import check_test
 
 def proc(config_file):
@@ -16,16 +17,21 @@ def proc(config_file):
     full_path = os.path.expandvars(config_file)
 
     conf_dict = read_config_file(full_path)
-    # removing the first two components so that they can be fed into functions
-    arg_dict  = dict(list(conf_dict.items())[2:])
+    
     # check the method implemented, currently just process
     match conf_dict['process']:
         case 'decode':
+            # removing the first two components so that the other arguments are passed correctly
+            arg_dict  = dict(list(conf_dict.items())[2:])
             if conf_dict['wavedump_edition'] == 2:
                 process_bin_WD2(**arg_dict)
             elif conf_dict['wavedump_edition'] == 1:
                 process_bin_WD1(**arg_dict)
             else:
                 raise RuntimeError(f"wavedump edition {conf_dict['wavedump_edition']} decoding isn't currently implemented.")
+        case 'calibrate':
+            # removing the first component so that the other arguments are passed correctly
+            arg_dict  = dict(list(conf_dict.items())[1:])
+            calibrate(**arg_dict)
         case default:
             raise RuntimeError(f"process {conf_dict['process']} not currently implemented.")
