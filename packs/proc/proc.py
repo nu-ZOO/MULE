a6ef342442384d5346abe620453df8ba55cc9e2c
Ian0sborne <ian.osborne@student.manchester.ac.uk>
2026-01-26 11:46:20 +0000

    handle incorrect key entries

--- packs/proc/proc.py
+++ packs/proc/proc.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import traceback
 
 from packs.core.io                import read_config_file
 from packs.proc.processing_utils  import process_bin_WD2
@@ -18,14 +20,20 @@ def proc(config_file):
     conf_dict = read_config_file(full_path)
     # removing the first two components so that they can be fed into functions
     arg_dict  = dict(list(conf_dict.items())[2:])
+
     # check the method implemented, currently just process
-    match conf_dict['process']:
-        case 'decode':
-            if conf_dict['wavedump_edition'] == 2:
-                process_bin_WD2(**arg_dict)
-            elif conf_dict['wavedump_edition'] == 1:
-                process_bin_WD1(**arg_dict)
-            else:
-                raise RuntimeError(f"wavedump edition {conf_dict['wavedump_edition']} decoding isn't currently implemented.")
-        case default:
-            raise RuntimeError(f"process {conf_dict['process']} not currently implemented.")
+    try:
+        match conf_dict['process']:
+            case 'decode':
+                if conf_dict['wavedump_edition'] == 2:
+                    process_bin_WD2(**arg_dict)
+                elif conf_dict['wavedump_edition'] == 1:
+                    process_bin_WD1(**arg_dict)
+                else:
+                    raise RuntimeError(f"wavedump edition {conf_dict['wavedump_edition']} decoding isn't currently implemented.")
+            case default:
+                raise RuntimeError(f"process {conf_dict['process']} not currently implemented.")
+    except KeyError as e:
+        print(f"\nError in the configuration file: missing required key {e} \n")
+        traceback.print_exc()
+        sys.exit(2)
