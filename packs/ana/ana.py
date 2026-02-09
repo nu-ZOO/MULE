1f8fa85b16121c9e402227df6a62e86240c2de52
Tedsmith100 <edward.smith@manchester.ac.uk>
2026-02-09 15:05:31 +0000

    add window args check

--- packs/ana/ana.py
+++ packs/ana/ana.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 from packs.core.io import read_config_file
-from packs.ana.analysis_utils import average_waveforms
+from packs.ana.analysis_utils import average_waveforms, window_overlap_check
 from packs.core.core_utils import check_test
 
 def ana(config_file : str) -> (np.ndarray):
@@ -11,6 +11,7 @@ def ana(config_file : str) -> (np.ndarray):
     
     conf_args = read_config_file(config_file)
     arg_dict = dict(list(conf_args.items())[:-1])
+    window_overlap_check(arg_dict)
 
     if isinstance(conf_args['files'], list):
         print('Averaging waveform....')
