2a2d2c0ee9b9a3d8d1be703d4321e51434cb76f0
Tedsmith100 <edward.smith@manchester.ac.uk>
2026-04-13 16:58:50 +0100

    correct path for check_save_path

--- packs/ana/ana.py
+++ packs/ana/ana.py
@@ -2,7 +2,8 @@ import numpy as np
 import os
 from packs.core.io import read_config_file
 from packs.ana.analysis_utils import average_waveforms, window_overlap_check
-from packs.core.core_utils import check_test, check_save_path
+from packs.core.core_utils import check_test
+from packs.proc.processing_utils import check_save_path
 import h5py
 
 def ana(config_file : str) -> (np.ndarray):
