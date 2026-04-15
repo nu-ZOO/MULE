0a4892aa6f3919f8ed3a37124117887ba8d9701b
Tedsmith100 <edward.smith@manchester.ac.uk>
2026-04-15 13:13:47 +0100

    clean up dictionary processing of config arguments

--- packs/ana/ana.py
+++ packs/ana/ana.py
@@ -14,33 +14,25 @@ def ana(config_file : str) -> (np.ndarray):
     conf_args = read_config_file(config_file)
     window_args = conf_args["window_args"]
 
-    waveform_args = {
-        "files": conf_args["files"],
-        "bin_size": conf_args["bin_size"],
-        "window_args": window_args,
-        "chunk_size": conf_args.get("chunk_size", 5),
-        "negative": conf_args.get("negative", True),
-        "baseline_mode": conf_args.get("baseline_mode", "median"),
-        "verbose": conf_args.get("verbose", 1),
-        "peak_threshold": conf_args.get("peak_threshold", 1000),
-        "suppression_threshold": conf_args.get("suppression_threshold", 10),
-    }
+    save_path = conf_args.pop('save_path')
+    overwrite = conf_args.pop('overwrite')
+
     window_overlap_check(window_args)
 
     if isinstance(conf_args["files"], list):
         print("Averaging waveform....")
 
-        avgwf = average_waveforms(**waveform_args)
+        avgwf = average_waveforms(**conf_args)
 
-        save_path = check_save_path(
-            conf_args["save_path"],
-            conf_args["overwrite"]
+        checked_save_path = check_save_path(
+            save_path,
+            overwrite
         )
 
-        with h5py.File(save_path, 'w') as f:     # Save as a h5
+        with h5py.File(checked_save_path, 'w') as f:     # Save as a h5
             f.create_dataset('Average_waveform', data=avgwf)
 
-        print(f"Saved to: {save_path}")
+        print(f"Saved to: {checked_save_path}")
 
     else:
         print("Please input files as a list.")
